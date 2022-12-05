89e04b5432d1023de7a8a13edb8f3bc135b1a8fd
abdorabee <abdorabee1134@gmail.com>
2022-12-05 16:19:46 +0200

    added the frontend and finished settingup it

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  <header>
+    <div className="container">
+      <Link to="/">
+        <h1>Abdo Workout buddy</h1>
+      </Link>
+    </div>
+  </header>;
+};
+
+export default Navbar;
