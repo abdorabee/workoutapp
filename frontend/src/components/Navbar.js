51dbab91e82f541a1bb3c137b76dfe2b75c4194c
abdorabee <abdorabee1134@gmail.com>
2022-12-07 15:19:08 +0200

    connteced the frontend with backend

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  <header>
-    <div className="container">
-      <Link to="/">
-        <h1>Abdo Workout buddy</h1>
-      </Link>
-    </div>
-  </header>;
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1> Rabee Workout Buddy</h1>
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
   return (
     <div className="home">
-      <h2>The home page</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails workout={workout} key={workout._id} />
+          ))}
+      </div>
     </div>
   );
 };
